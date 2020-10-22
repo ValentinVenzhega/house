55ebd78f57efcac1118867806a65775e2b723708
Valentin <valek917@yandex.ru>
2020-10-22 18:38:06 +0300

    commit1

---
+++ js/main.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const burgerMenu = document.querySelector('.menu__burger'),
+      menu = document.querySelector('.menu'),
+      menuItem = document.querySelectorAll('.menu__i');
+
+
+//открытие и закртие меню и бургера
+
+burgerMenu.addEventListener('click', () => {
+   burgerMenu.classList.toggle('menu__burger_active');
+   menu.classList.toggle('menu_active');
+});
+
+
+//  закрытие меню и бургера по клику на элемент меню
+
+menuItem.forEach(function (e) { 
+   e.addEventListener('click', () => {
+      burgerMenu.classList.toggle('menu__burger_active');
+      menu.classList.toggle('menu_active');
+   });
+});
+
+
+$(document).ready(function(){
+   $("#phone").mask("(999) 999-9999");
+});
+
+$(document).scroll(function(){
+   if ($(this).scrollTop()
+   );
+});
+
+
+
+
