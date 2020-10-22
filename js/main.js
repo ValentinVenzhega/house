9150d520522f6735215fea433380e89cf6b5d9df
Valentin <valek917@yandex.ru>
2020-10-22 19:27:16 +0300

    commit2

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@
 
 const burgerMenu = document.querySelector('.menu__burger'),
       menu = document.querySelector('.menu'),
-      menuItem = document.querySelectorAll('.menu__i');
+      menuItem = document.querySelectorAll('.menu__i'),
+      menuList = document.querySelector('.menu__list');
 
 
 //открытие и закртие меню и бургера
@@ -10,6 +11,7 @@ const burgerMenu = document.querySelector('.menu__burger'),
 burgerMenu.addEventListener('click', () => {
    burgerMenu.classList.toggle('menu__burger_active');
    menu.classList.toggle('menu_active');
+   document.body.style = 'overflow: hidden';
 });
 
 
@@ -19,6 +21,7 @@ menuItem.forEach(function (e) {
    e.addEventListener('click', () => {
       burgerMenu.classList.toggle('menu__burger_active');
       menu.classList.toggle('menu_active');
+      document.body.style = 'overflow: visible';
    });
 });
 
